441ef13467bf34091aaab619a3c6b5dcaecf4011
PatrickGharib <36083375+PatrickGharib@users.noreply.github.com>
2019-02-14 15:53:51 -0700

    remove variables/setters/getters
    mana, totalMana, and money are never used anywhere else. They are a Product of speculative generality. Remove Parameter refactoring.

--- Player.java
+++ Player.java
@@ -13,9 +13,7 @@ public class Player {
     private int str;
     private int intel;
     private int evs;
-    private int mana;
-    private int totalMana;
-    private int Money; 
+  
 
     //Constructor
     public Player() {
@@ -23,28 +21,20 @@ public class Player {
         this.str = -1;
         this.intel = -1;
         this.evs = -1;
-        this.mana = -1; 
-        this.totalMana = -1;
-        this.money = -1;
+
     }
     //Set functions for player attributes
     public void sethp(int hp){ this.hp = hp; }
     public void setstr(int str){ this.str = str; }
     public void setintel(int intel){ this.intel = intel; }
     public void setevs(int evs){this.evs =evs;}
-    public void setMana(int mana){ this.mana = mana; }
-	public void setTotalMana(int totalMana) { this.totalMana = totalMana; }
-    public void setMoney(int money){ this.money = money; }
+
 
     //Get functions for player attributes
     public int gethp(){ return this.hp; }
     public int getstr(){ return this.str ;}
     public int getintel(){ return this.intel; }
     public int getevs(){return this.evs;}
-    
-    public int getMana(){ return this.mana; }
-	public int getTotalMana(){ return this.totalMana; }
-    public int getMoney(){ return this.money;}
     public char getPlayerClass(){ return this.playerClass; }
     public void displayAttributes(){
         System.out.println( "\nPlayer \n"    +
